5d61a5fa26e891d44db4a576ad0de71e7c59331d
Roi Levy <roilevi65xxx@gmail.com>
2019-05-30 17:14:52 +0300

    Modified gitignore

--- src/nlp_util.py
+++ src/nlp_util.py
@@ -9,35 +9,33 @@ otherwise use "en_core_web_sm"
 nlp = spacy.load("en_core_web_sm")
 
 
-'''
-Tokenizing a sentence using spaCy model
-And puts them in a list grouped by part of speech
-'''
 def tokenize(sentence):
+    '''
+    Tokenizing a sentence using spaCy model
+    And puts them in a list grouped by part of speech
+    '''
     doc = nlp(sentence)
     tags = []
-    if doc != [] :
+    if doc != []:
         tags.append([doc[0].pos_])
-        for w in doc :
+        for w in doc:
             if w.pos_ == "PUNCT" or w.lemma_ == "be" or w.pos_ == "DET":
                 continue
-            istag=False 
-            for t in tags :
-                if t[0] == w.pos_ :
+            istag = False
+            for t in tags:
+                if t[0] == w.pos_:
                     t.append(w.text)
-                    istag=True
-            if istag==False :
-                tag=[w.pos_,w.text]
+                    istag = True
+            if istag == False:
+                tag = [w.pos_, w.text]
                 tags.append(tag)
     return tags
 
-'''
-Using spaCy model to compare between words
-Added warning ignore in orfer to ignore small model warnings
-'''
-def compare(word1,word2):
+def compare(word1, word2):
+    '''
+    Using spaCy model to compare between words
+    Added warning ignore in orfer to ignore small model warnings
+    '''
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         return nlp(word1).similarity(nlp(word2))
-
-
