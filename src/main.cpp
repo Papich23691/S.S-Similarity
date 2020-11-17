ece7adb72d4c33be32b9ed05293093a565eb88e5
Roi Levy <roilevi65xxx@gmail.com>
2020-11-17 21:17:48 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,13 @@
 
 using namespace std;
 
-int main(int argc,char ** argv) {
+int main(int argc,char ** argv) 
+{
   /* Quick Examples */
   nlp n;
-double res1 = n.semantic_similarity("Midday is 12 oclock in the middle of the day.","Noon is 12 oclock in the middle of the day.");
+  double res1 = n.semantic_similarity("Midday is 12 oclock in the middle of the day.","Noon is 12 oclock in the middle of the day.");
 
- printf("%f \n",res1);
+  printf("%f \n",res1);
  
   return 0;
 }
